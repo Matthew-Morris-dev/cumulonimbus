a7341bd161906e931cb7f350139bc3898d6ee4b4
Matthew Morris <m.morris9491@gmail.com>
2019-07-27 19:30:01 +0200

    Added day night cycle

--- Prototype1_Group6/Assets/SimpleRotate.cs
+++ Prototype1_Group6/Assets/SimpleRotate.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class SimpleRotate : MonoBehaviour
 {
 
-    float rotateSpeed = 1;
+    public float rotateSpeed = 1;
+
+    public GameObject sun;
+    public GameObject moon;
+    public GameObject[] stars;
+
+    private bool starsSetActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +27,22 @@ public class SimpleRotate : MonoBehaviour
     void BasicRotate()
     {
         this.transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+        sun.transform.up = Vector3.up;
+        moon.transform.up = Vector3.up;
+        if(!starsSetActive && this.transform.eulerAngles.z >= 51)
+        {
+            //Debug.LogError("we enter here");
+            SetStarsActive();
+        }
+    }
+
+    void SetStarsActive()
+    {
+        for(int i = 0; i < stars.Length; i++)
+        {
+            //Debug.LogError("We set star active");
+            stars[i].SetActive(true);
+        }
+        starsSetActive = true;
     }
 }
